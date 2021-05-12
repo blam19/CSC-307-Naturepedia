b89b8fa37a27d47a9d767e96191950b6f4b72ad2
kenneth <kyleandken@yahoo.com>
2021-05-12 12:50:59 -0700

    contact page GOOD

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -1,9 +1,59 @@
 import React from "react";
+import "./index.css";
+
+import { Link } from "react-router-dom";
+
+import person from '../person.jpg';
+
+function Bailey(){
+    return(
+        <div className="Bailey">
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gridGap: 20 }}>
+            <div> I am Bailey</div> 
+            <div> I am Brandon</div> 
+            <div> I am Dim</div>
+            <div> I am Ken</div>
+
+            <div> Email: bailbail@Naturepedia.com</div>
+            <div> Email: branbran@Naturepedia.com</div>
+            <div> Email: dimdim@Naturepedia.com</div>
+            <div> Email: kenken@Naturepedia.com</div>   
+        </div>
+      </div>
+    )
+}
+
+function Images(){
+    return(
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gridGap: 20 }}>
+            <img src={person} className="Bails"/>
+            <img src={person} className="Brandon"/>
+            <img src={person} className="Dim" />
+            <img src={person} className="Kenneth" />
+        </div>
+    )
+}
 
 const ContactPage = () => {
     return (
         <div>
             <h3>CONTACT PAGE</h3>
+            <div>
+                    {/* <button>Data</button> */}
+                    {/* <button>Contact Us</button> */}
+                    <Link to="/data"> Data </Link>
+                    <Link to="/contact"> Contact Us </Link>
+                    <Link to="/"> Home </Link>
+                    <Link to="/faq"> FAQ </Link>
+                    {/* <button>Home</button>
+                    <button>FAQ</button> */}
+            </div>
+            <div>
+                <Images></Images>
+            </div>
+            <div>
+                <Bailey></Bailey>
+            </div>
         </div>
     );
 }
