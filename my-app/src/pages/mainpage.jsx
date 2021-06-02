3d07f8e6d86b66954b2ad6a0bad04f77e8f18d0f
blam19 <blam19@calpoly.edu>
2021-06-01 20:46:39 -0700

    added main page stuff

--- my-app/src/pages/mainpage.jsx
+++ my-app/src/pages/mainpage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import tree from '../tree.png';
 
 const MainPage = () => {
     return (
         <div className="App">
             <header className="App-header">
                 
-                <h1>Naturepedia</h1>
+                <h1 class="title" style = {{fontSize : 100 }}>Naturepedia</h1>
                 <div>
                     {/* <button>Data</button> */}
                     {/* <button>Contact Us</button> */}
@@ -19,6 +20,21 @@ const MainPage = () => {
                     <button>FAQ</button> */}
                 </div>
                 <p></p>
+                <div>
+                    <img src={tree}/>
+                    <div>
+                        <h1 class="mission" style = {{fontSize : 50 }}>
+                            Mission: 
+                        </h1>
+                        <div class="missionDesc" style = {{ marginLeft : 370, marginRight : 370, fontSize : 30 }}>
+                            Nature underpins our very existence. Nature is what we eat, what we drink, what we breathe and more. 
+                            Because of how nature is so free, people often take it for granted and don't appreciate it.
+                            Our group believes that everyone should have some knowledge about nature and how limited it can be. 
+                            We aim to provide information of some of the most well-known items of nature to the public.
+                        </div>
+                        
+                    </div>
+                </div>
 
             </header>
         </div>
